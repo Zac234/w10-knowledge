fdd54832dfee70bfb27b4b597f1229f73a8cc886
Zach <zacbenincasa@gamil.com>
2022-07-12 17:29:41 -0600

    added steps 1-7

---
+++ W/js/app.js
@@ -0,0 +1,9 @@
+let user_name = ["doomguy324", "firelord991", "winnerman9000", "jokeguy675", "dadbod380"]
+let lower_user_name=user_name.tolowercase();
+let counter = 0
+
+while(counter <5){
+    console.log(lower_user_name)
+    console.log(lower_user_name.include("winner"))
+    counter=counter+1
+}
